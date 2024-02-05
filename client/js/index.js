7527927c7d1287c76872bafba784f14096f2cfcc
Nakwonyoon <yoonnw1000@gmail.com>
2024-02-05 11:08:51 +0900

    finalize footer & mega menu

--- client/js/index.js
+++ client/js/index.js
@@ -21,3 +21,11 @@ const swiper = new Swiper('.swiper', {
   });
 
   
+  document.addEventListener("DOMContentLoaded", function(){
+    /////// Prevent closing from click inside dropdown
+    document.querySelectorAll('.dropdown-menu').forEach(function(element){
+      element.addEventListener('click', function (e) {
+        e.stopPropagation();
+      });
+    })
+}); 
